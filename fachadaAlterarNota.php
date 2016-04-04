a73bae7820d398fc35b5f7da9d2aa7bd4979abd9
biao <biao@localhost.localdomain>
2016-04-04 15:33:06 -0300

    calculando media forum

--- fachadaAlterarNota.php
+++ fachadaAlterarNota.php
@@ -7,7 +7,7 @@
         if (!isset($_SESSION)) session_start();
         $id_turma = trim($_POST["id_turma"]);
         $matricula_aluno = trim($_POST["matricula_aluno"]);
-        echo $matricula_aluno;
+        //echo $matricula_aluno;
         $qtd_ava_forum = trim($_POST["qtd_ava_forum"]);
         
         // chamando a função query da classe banco para adicionar ao banco de dados
@@ -23,7 +23,14 @@
             //echo $b;
             noQuery($b);
             $b = "";
-            
+            $query = "select sum(nota) as nota from ava_forum where aluno_turma_turma_id_turma = $id_turma and aluno_turma_aluno_matricula_aluno = $matricula_aluno";
+            //echo $query;
+            $rs = Select ($query);
+            $row = mysql_fetch_array($rs);
+            $media_forum = $row['nota']/$qtd_ava_forum;
+            //echo $media_forum;
+            $query = "update aluno_turma set med_forum = $media_forum where turma_id_turma = $id_turma and aluno_matricula_aluno = $matricula_aluno";
+            noQuery($query);
         }
         
         $_SESSION['id_turma'] =  $id_turma;
