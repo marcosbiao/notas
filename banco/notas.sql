430b38bbf5de663f260a77a8d27465f533e9a66a
biao <biao@fedora>
2016-04-07 14:36:12 -0300

    script banco de dados

--- banco/notas.sql
+++ banco/notas.sql
@@ -2,8 +2,8 @@
 -- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 07-Mar-2016 às 12:28
+-- Host: localhost
+-- Generation Time: 06-Abr-2016 às 22:47
 -- Versão do servidor: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -27,20 +27,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `adm` (
-  `id_adm` int(11) NOT NULL AUTO_INCREMENT,
+  `id_adm` int(11) NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
   `login` varchar(45) DEFAULT NULL,
   `senha` varchar(45) DEFAULT NULL,
   `nivel` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_adm`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `adm`
 --
 
 INSERT INTO `adm` (`id_adm`, `nome`, `login`, `senha`, `nivel`) VALUES
-(1, 'biao', 'root', 'root', 1);
+(0, 'root', 'root', 'root', 1);
 
 -- --------------------------------------------------------
 
@@ -49,25 +49,24 @@ INSERT INTO `adm` (`id_adm`, `nome`, `login`, `senha`, `nivel`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `aluno` (
-  `id_aluno` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula_aluno` int(11) NOT NULL,
   `nome_aluno` varchar(45) DEFAULT NULL,
   `email_aluno` varchar(60) DEFAULT NULL,
-  `matricula_aluno` int(11) DEFAULT NULL,
   `cpf_aluno` int(11) DEFAULT NULL,
   `polo` varchar(45) DEFAULT NULL,
   `nivel` int(11) DEFAULT NULL,
   `ativo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_aluno`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  PRIMARY KEY (`matricula_aluno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `aluno`
 --
 
-INSERT INTO `aluno` (`id_aluno`, `nome_aluno`, `email_aluno`, `matricula_aluno`, `cpf_aluno`, `polo`, `nivel`, `ativo`) VALUES
-(2, 'aluno', 'jidhfis@hotmail.com', 101, 101, 'Ipira', 4, 1),
-(3, 'aluno2', 'jdfvh@hotmail.com', 303, 303, 'ipira', 4, 1),
-(4, 'aluno3', 'jdfwdvh@hotmail.com', 404, 404, 'ipira', 4, 1);
+INSERT INTO `aluno` (`matricula_aluno`, `nome_aluno`, `email_aluno`, `cpf_aluno`, `polo`, `nivel`, `ativo`) VALUES
+(101, 'aluno001', 'aluno001@gmail.com', 101, 'Ipira', 4, 1),
+(202, 'aluno002', 'aluno002@gmail.com', 202, 'Ipira', 4, 1),
+(303, 'aluno003', 'aluno003@gmail.com', 303, 'Ipira', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -76,30 +75,69 @@ INSERT INTO `aluno` (`id_aluno`, `nome_aluno`, `email_aluno`, `matricula_aluno`,
 --
 
 CREATE TABLE IF NOT EXISTS `aluno_turma` (
-  `aluno_id_aluno` int(11) NOT NULL,
-  `nota1` float DEFAULT NULL,
-  `nota2` float DEFAULT NULL,
-  `nota3` float DEFAULT NULL,
-  `nota4` float DEFAULT NULL,
-  `media` float DEFAULT NULL,
   `turma_id_turma` int(11) NOT NULL,
-  `turma_semestre_id_semestre` int(11) NOT NULL,
   `turma_professor_id_professor` int(11) NOT NULL,
   `turma_disciplina_id_disciplina` int(11) NOT NULL,
-  PRIMARY KEY (`aluno_id_aluno`,`turma_id_turma`,`turma_semestre_id_semestre`,`turma_professor_id_professor`,`turma_disciplina_id_disciplina`),
-  KEY `fk_aluno_has_turma_aluno1_idx` (`aluno_id_aluno`),
-  KEY `fk_aluno_turma_turma1_idx` (`turma_id_turma`,`turma_semestre_id_semestre`,`turma_professor_id_professor`,`turma_disciplina_id_disciplina`)
+  `turma_semestre_id_semestre` int(11) NOT NULL,
+  `aluno_matricula_aluno` int(11) NOT NULL,
+  `med_forum` float DEFAULT NULL,
+  `med_presencial` float DEFAULT NULL,
+  `media` float DEFAULT NULL,
+  `situacao` varchar(45) DEFAULT NULL,
+  `nota_final` float DEFAULT NULL,
+  `media_final` float DEFAULT NULL,
+  PRIMARY KEY (`turma_id_turma`,`turma_professor_id_professor`,`turma_disciplina_id_disciplina`,`turma_semestre_id_semestre`,`aluno_matricula_aluno`),
+  KEY `fk_aluno_has_turma_turma1_idx` (`turma_id_turma`,`turma_professor_id_professor`,`turma_disciplina_id_disciplina`,`turma_semestre_id_semestre`),
+  KEY `fk_aluno_turma_aluno1_idx` (`aluno_matricula_aluno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `aluno_turma`
 --
 
-INSERT INTO `aluno_turma` (`aluno_id_aluno`, `nota1`, `nota2`, `nota3`, `nota4`, `media`, `turma_id_turma`, `turma_semestre_id_semestre`, `turma_professor_id_professor`, `turma_disciplina_id_disciplina`) VALUES
-(2, 4, 2, 1, 0, 1.75, 1, 3, 7, 5),
-(2, NULL, NULL, NULL, NULL, NULL, 7, 4, 7, 5),
-(3, 8, 8, 9, 1, 6.5, 1, 3, 7, 5),
-(4, 1, 10, 4, 0, 3.75, 1, 3, 7, 5);
+INSERT INTO `aluno_turma` (`turma_id_turma`, `turma_professor_id_professor`, `turma_disciplina_id_disciplina`, `turma_semestre_id_semestre`, `aluno_matricula_aluno`, `med_forum`, `med_presencial`, `media`, `situacao`, `nota_final`, `media_final`) VALUES
+(1, 6, 1, 1, 101, 2.66667, NULL, NULL, NULL, NULL, NULL),
+(1, 6, 1, 1, 202, 2, NULL, NULL, NULL, NULL, NULL),
+(1, 6, 1, 1, 303, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 6, 1, 1, 101, 5, NULL, NULL, NULL, NULL, NULL),
+(3, 6, 2, 2, 101, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ava_forum`
+--
+
+CREATE TABLE IF NOT EXISTS `ava_forum` (
+  `id_ava_forum` int(11) NOT NULL,
+  `nota` float DEFAULT NULL,
+  `aluno_turma_turma_id_turma` int(11) NOT NULL,
+  `aluno_turma_turma_professor_id_professor` int(11) NOT NULL,
+  `aluno_turma_turma_disciplina_id_disciplina` int(11) NOT NULL,
+  `aluno_turma_turma_semestre_id_semestre` int(11) NOT NULL,
+  `aluno_turma_aluno_matricula_aluno` int(11) NOT NULL,
+  PRIMARY KEY (`id_ava_forum`,`aluno_turma_turma_id_turma`,`aluno_turma_turma_professor_id_professor`,`aluno_turma_turma_disciplina_id_disciplina`,`aluno_turma_turma_semestre_id_semestre`,`aluno_turma_aluno_matricula_aluno`),
+  KEY `fk_ava_forum_aluno_turma1_idx` (`aluno_turma_turma_id_turma`,`aluno_turma_turma_professor_id_professor`,`aluno_turma_turma_disciplina_id_disciplina`,`aluno_turma_turma_semestre_id_semestre`,`aluno_turma_aluno_matricula_aluno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `ava_forum`
+--
+
+INSERT INTO `ava_forum` (`id_ava_forum`, `nota`, `aluno_turma_turma_id_turma`, `aluno_turma_turma_professor_id_professor`, `aluno_turma_turma_disciplina_id_disciplina`, `aluno_turma_turma_semestre_id_semestre`, `aluno_turma_aluno_matricula_aluno`) VALUES
+(1, 3, 1, 6, 1, 1, 101),
+(1, 1, 1, 6, 1, 1, 202),
+(1, 2, 1, 6, 1, 1, 303),
+(1, 7, 2, 6, 1, 1, 101),
+(1, NULL, 3, 6, 2, 2, 101),
+(2, 1, 1, 6, 1, 1, 101),
+(2, 2, 1, 6, 1, 1, 202),
+(2, 3, 1, 6, 1, 1, 303),
+(2, 3, 2, 6, 1, 1, 101),
+(2, NULL, 3, 6, 2, 2, 101),
+(3, 4, 1, 6, 1, 1, 101),
+(3, 3, 1, 6, 1, 1, 202),
+(3, NULL, 1, 6, 1, 1, 303);
 
 -- --------------------------------------------------------
 
@@ -113,15 +151,16 @@ CREATE TABLE IF NOT EXISTS `disciplina` (
   `cod` varchar(7) DEFAULT NULL,
   `ativo` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_disciplina`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Extraindo dados da tabela `disciplina`
 --
 
 INSERT INTO `disciplina` (`id_disciplina`, `nome_disciplina`, `cod`, `ativo`) VALUES
-(5, 'Disciplina', 'cet-151', 1),
-(6, 'disciplina2', 'cet-002', 1);
+(1, 'Disciplina 01', 'ead001', 1),
+(2, 'Disciplina 02', 'cet111', 1),
+(3, 'Calculo I', 'cet-111', 1);
 
 -- --------------------------------------------------------
 
@@ -137,15 +176,15 @@ CREATE TABLE IF NOT EXISTS `professor` (
   `nivel` int(11) DEFAULT NULL,
   `ativo` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_professor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Extraindo dados da tabela `professor`
 --
 
 INSERT INTO `professor` (`id_professor`, `nome_professor`, `matricula_professor`, `cpf_professor`, `nivel`, `ativo`) VALUES
-(7, 'professor', 1010, 1010, 2, 1),
-(8, 'prof2', 2020, 2020, 2, 1);
+(6, 'professor 01', 1010, 1010, 2, 1),
+(7, 'Marcos', 4040, 4040, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -158,46 +197,46 @@ CREATE TABLE IF NOT EXISTS `semestre` (
   `nome_semestre` varchar(6) NOT NULL,
   `ativo` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_semestre`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Extraindo dados da tabela `semestre`
 --
 
 INSERT INTO `semestre` (`id_semestre`, `nome_semestre`, `ativo`) VALUES
-(3, '2015.2', 1),
-(4, '2016.1', 1);
+(1, '2015.2', 1),
+(2, '2016.1', 1),
+(3, '2015.1', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `turmas`
+-- Estrutura da tabela `turma`
 --
 
-CREATE TABLE IF NOT EXISTS `turmas` (
+CREATE TABLE IF NOT EXISTS `turma` (
   `id_turma` int(11) NOT NULL AUTO_INCREMENT,
-  `senha` varchar(45) NOT NULL,
-  `semestre_id_semestre` int(11) NOT NULL,
   `professor_id_professor` int(11) NOT NULL,
   `disciplina_id_disciplina` int(11) NOT NULL,
-  PRIMARY KEY (`id_turma`,`semestre_id_semestre`,`professor_id_professor`,`disciplina_id_disciplina`),
-  KEY `fk_turma_semestre1_idx` (`semestre_id_semestre`),
-  KEY `fk_turma_professor1_idx` (`professor_id_professor`),
-  KEY `fk_turma_disciplina1_idx` (`disciplina_id_disciplina`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+  `semestre_id_semestre` int(11) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `numero_ava_forum` int(11) NOT NULL,
+  `numero_ava_presencial` int(11) NOT NULL,
+  PRIMARY KEY (`id_turma`,`professor_id_professor`,`disciplina_id_disciplina`,`semestre_id_semestre`),
+  KEY `fk_turma_professor_idx` (`professor_id_professor`),
+  KEY `fk_turma_disciplina1_idx` (`disciplina_id_disciplina`),
+  KEY `fk_turma_semestre1_idx` (`semestre_id_semestre`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
--- Extraindo dados da tabela `turmas`
+-- Extraindo dados da tabela `turma`
 --
 
-INSERT INTO `turmas` (`id_turma`, `senha`, `semestre_id_semestre`, `professor_id_professor`, `disciplina_id_disciplina`) VALUES
-(1, '1111', 3, 7, 5),
-(2, '1111', 4, 7, 6),
-(7, '1111', 4, 7, 5),
-(8, '1111', 4, 7, 5),
-(9, '1111', 4, 7, 5),
-(10, '1111', 4, 7, 5),
-(11, '45712', 4, 7, 6);
+INSERT INTO `turma` (`id_turma`, `professor_id_professor`, `disciplina_id_disciplina`, `semestre_id_semestre`, `senha`, `numero_ava_forum`, `numero_ava_presencial`) VALUES
+(1, 6, 1, 1, '1234', 3, 3),
+(2, 6, 1, 1, '9090', 2, 2),
+(3, 6, 2, 2, '1111', 2, 2),
+(4, 7, 3, 3, '1234567', 2, 3);
 
 --
 -- Constraints for dumped tables
@@ -207,15 +246,21 @@ INSERT INTO `turmas` (`id_turma`, `senha`, `semestre_id_semestre`, `professor_id
 -- Limitadores para a tabela `aluno_turma`
 --
 ALTER TABLE `aluno_turma`
-  ADD CONSTRAINT `fk_aluno_has_turma_aluno1` FOREIGN KEY (`aluno_id_aluno`) REFERENCES `aluno` (`id_aluno`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_aluno_turma_turma1` FOREIGN KEY (`turma_id_turma`, `turma_semestre_id_semestre`, `turma_professor_id_professor`, `turma_disciplina_id_disciplina`) REFERENCES `turmas` (`id_turma`, `semestre_id_semestre`, `professor_id_professor`, `disciplina_id_disciplina`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_aluno_has_turma_turma1` FOREIGN KEY (`turma_id_turma`, `turma_professor_id_professor`, `turma_disciplina_id_disciplina`, `turma_semestre_id_semestre`) REFERENCES `turma` (`id_turma`, `professor_id_professor`, `disciplina_id_disciplina`, `semestre_id_semestre`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_aluno_turma_aluno1` FOREIGN KEY (`aluno_matricula_aluno`) REFERENCES `aluno` (`matricula_aluno`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `turmas`
+-- Limitadores para a tabela `ava_forum`
 --
-ALTER TABLE `turmas`
+ALTER TABLE `ava_forum`
+  ADD CONSTRAINT `fk_ava_forum_aluno_turma1` FOREIGN KEY (`aluno_turma_turma_id_turma`, `aluno_turma_turma_professor_id_professor`, `aluno_turma_turma_disciplina_id_disciplina`, `aluno_turma_turma_semestre_id_semestre`, `aluno_turma_aluno_matricula_aluno`) REFERENCES `aluno_turma` (`turma_id_turma`, `turma_professor_id_professor`, `turma_disciplina_id_disciplina`, `turma_semestre_id_semestre`, `aluno_matricula_aluno`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `turma`
+--
+ALTER TABLE `turma`
   ADD CONSTRAINT `fk_turma_disciplina1` FOREIGN KEY (`disciplina_id_disciplina`) REFERENCES `disciplina` (`id_disciplina`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_turma_professor1` FOREIGN KEY (`professor_id_professor`) REFERENCES `professor` (`id_professor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_turma_professor` FOREIGN KEY (`professor_id_professor`) REFERENCES `professor` (`id_professor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_turma_semestre1` FOREIGN KEY (`semestre_id_semestre`) REFERENCES `semestre` (`id_semestre`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
